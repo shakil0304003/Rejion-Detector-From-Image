867c1458aa391e6c592682b6041dae9bfc591ff9
unknown <Shakil@cuet-same.(none)>
2012-05-05 10:26:14 +0600

    Init

---
+++ Rejion Detector/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Rejion_Detector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.lblPoints = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(25, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1133, 661);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 681);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Select";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(433, 682);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Detect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(157, 685);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "#971A1E";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(118, 687);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Color:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(267, 687);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Min Size :";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(327, 685);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "2";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(25, 747);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(177, 725);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(383, 199);
+            this.listBox1.TabIndex = 8;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(25, 775);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(68, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Show Fill";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // lblPoints
+            // 
+            this.lblPoints.AutoSize = true;
+            this.lblPoints.Location = new System.Drawing.Point(604, 725);
+            this.lblPoints.Name = "lblPoints";
+            this.lblPoints.Size = new System.Drawing.Size(63, 13);
+            this.lblPoints.TabIndex = 10;
+            this.lblPoints.Text = "Total Points";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1178, 938);
+            this.Controls.Add(this.lblPoints);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label lblPoints;
+    }
+}
+
---
+++ Rejion Detector/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Rejion_Detector
+{
+    public partial class Form1 : Form
+    {
+        private string _sourcePath = "";
+        private Bitmap btp;
+        private Bitmap destinationBtp;
+        private Color color;
+        private int _width = 0, _height = 0;
+        private Color _destinationColor = Color.Black;
+        private int _maxDif = 150;
+        private int _initDif = 150;
+        private int _region;
+        private int _minSize = 0;
+        private List<Color> colors = new List<Color>();
+        bool[][] _flag;
+        List<List<Point>> _allPoints = new List<List<Point>>();
+
+        public Form1()
+        {
+            colors.Add(Color.Black);
+            colors.Add(Color.Yellow);
+            colors.Add(Color.Green);
+            colors.Add(Color.Red);
+            colors.Add(Color.Blue);
+            colors.Add(Color.Chocolate);
+            colors.Add(Color.DarkBlue);
+            colors.Add(Color.DarkGreen);
+            colors.Add(Color.DarkOrange);
+            colors.Add(Color.DarkRed);
+            colors.Add(Color.DarkViolet);
+            colors.Add(Color.ForestGreen);
+            colors.Add(Color.Gold);
+            colors.Add(Color.LightPink);
+            colors.Add(Color.LightSteelBlue);
+            colors.Add(Color.Pink);
+            colors.Add(Color.Purple);
+            colors.Add(Color.Silver);
+            colors.Add(Color.PaleVioletRed);
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            _sourcePath = openFileDialog1.FileName;
+            pictureBox1.Image = new Bitmap(_sourcePath);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            btp = new Bitmap(pictureBox1.Image);
+            _minSize = Convert.ToInt16(textBox2.Text);
+            _width = btp.Width;
+            _height = btp.Height;
+            color = ColorTranslator.FromHtml(textBox1.Text);
+            Convert11();
+            pictureBox1.Image = destinationBtp;
+        }
+
+        private void Convert11()
+        {
+            _region = 0;
+
+            _flag = new bool[_height][];
+            destinationBtp = new Bitmap(_width, _height);
+
+            for (int i = 0; i < _height; i++)
+            {
+                _flag[i] = new bool[_width];
+                for (int j = 0; j < _width; j++)
+                {
+                    _flag[i][j] = false;
+                    destinationBtp.SetPixel(j, i, Color.White);
+                }
+            }
+
+            _allPoints = new List<List<Point>>();
+
+            for (int i = 0; i < _height; i++)
+                for (int j = 0; j < _width; j++)
+                    if (_flag[i][j] == false)
+                    {
+                        Color currentColor = btp.GetPixel(j, i);
+                        int diff = Math.Abs(currentColor.R - color.R) + Math.Abs(currentColor.G - color.G) + Math.Abs(currentColor.B - color.B);
+
+                        if (diff <= _initDif)
+                        {
+                            BFS(j, i);
+                        }
+                    }
+            MessageBox.Show(_region.ToString());
+
+            comboBox1.Items.Clear();
+            comboBox1.Items.Add("Select");
+
+            for (int i = 0; i < _allPoints.Count; i++)
+            {
+                comboBox1.Items.Add((i + 1).ToString());
+            }
+        }
+
+
+        private void BFS(int x, int y)
+        {
+            List<Point> points = new List<Point>();
+
+            _destinationColor = colors[_region % colors.Count];
+
+            points.Add(new Point(x, y));
+            _flag[y][x] = true;
+
+            int current = 0;
+
+            while (current < points.Count)
+            {
+                x = points[current].X;
+                y = points[current].Y;
+                Color cc = color; //btp.GetPixel(x, y);
+
+                for (int i = -1; i <= 1; i++)
+                    for (int j = -1; j <= 1; j++)
+                        if (x + j >= 0 && x + j < _width && y + i >= 0 && y + i < _height && _flag[y + i][x + j] == false)
+                        {
+                            Color currentColor = btp.GetPixel(x + j, y + i);
+
+                            if (!(currentColor.R == Color.White.R && currentColor.G == Color.White.G && currentColor.B == Color.White.B))
+                            {
+                                int diff = Math.Abs(currentColor.R - cc.R) + Math.Abs(currentColor.G - cc.G) + Math.Abs(currentColor.B - cc.B);
+                                if (diff <= _maxDif)
+                                {
+                                    _flag[y + i][x + j] = true;
+                                    points.Add(new Point(x + j, y + i));
+                                }
+                            }
+                        }
+
+                current++;
+            }
+
+            if (current >= _minSize)
+            {
+                for (int i = 0; i < current; i++)
+                {
+                    destinationBtp.SetPixel(points[i].X, points[i].Y, _destinationColor);
+                }
+
+                _allPoints.Add(points);
+                _region++;
+            }
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int select = comboBox1.SelectedIndex - 1;
+
+            if (select >= 0)
+            {
+                for (int i = 0; i < _height; i++)
+                {
+                    for (int j = 0; j < _width; j++)
+                    {
+                        btp.SetPixel(j, i, Color.White);
+                    }
+                }
+
+                List<Point> points = new List<Point>();
+
+                for (int i = 0; i < _allPoints[select].Count; i++)
+                {
+                    if (checkBox1.Checked)
+                        btp.SetPixel(_allPoints[select][i].X, _allPoints[select][i].Y, Color.Red);
+
+                    int count = 0;
+
+                    for (int i1 = -1; i1 <= 1; i1++)
+                        for (int j1 = -1; j1 <= 1; j1++)
+                            if (_allPoints[select][i].X + j1 >= 0 && _allPoints[select][i].X + j1 < _width && _allPoints[select][i].Y + i1 >= 0 && _allPoints[select][i].Y + i1 < _height)
+                            {
+                                if (_allPoints[select].Contains(new Point(_allPoints[select][i].X + j1, _allPoints[select][i].Y + i1)))
+                                    count++;
+                            }
+
+                    if (count < 9)
+                        points.Add(new Point(_allPoints[select][i].X, _allPoints[select][i].Y));
+                }
+
+                listBox1.Items.Clear();
+
+                for (int i = 0; i < points.Count; i++)
+                {
+                    if (!checkBox1.Checked)
+                        btp.SetPixel(points[i].X, points[i].Y, Color.Red);
+                    listBox1.Items.Add(points[i].X + "," + points[i].Y);
+                }
+
+                lblPoints.Text = listBox1.Items.Count.ToString();
+                pictureBox1.Image = btp;
+            }
+            else
+            {
+                pictureBox1.Image = destinationBtp;
+            }
+        }
+    }
+}
